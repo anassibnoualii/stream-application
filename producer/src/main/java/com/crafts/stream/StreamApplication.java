f3c44172450bab4ffb7a3203e80211b28d7f4e0c
Anass ibnouali <anass.ibnou-ali@alithya.com>
2023-12-06 19:49:24 +0100

    app: Add Flux delay elements

--- producer/src/main/java/com/crafts/stream/StreamApplication.java
+++ producer/src/main/java/com/crafts/stream/StreamApplication.java
@@ -1,13 +1,14 @@
 package com.crafts.stream;
 
 import com.crafts.stream.kafka.KafkaProducer;
-import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @Slf4j
@@ -17,8 +18,7 @@ public class StreamApplication implements CommandLineRunner {
 
   public StreamApplication(KafkaProducer kafkaProducer) {
     this.kafkaProducer = kafkaProducer;
-    this.webClient =
-        WebClient.builder().baseUrl("https://stream.wikimedia.org/v2/stream/recentchange").build();
+    this.webClient = WebClient.builder().baseUrl("https://stream.wikimedia.org/v2/stream/recentchange").build();
   }
 
   public static void main(String[] args) {
@@ -27,16 +27,19 @@ public class StreamApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    Flux.interval(Duration.ofSeconds(15))
-        .flatMap(tick -> getStreamData())
-        .doOnNext(s -> {
-          log.info("data from stream: {}", s);
-          kafkaProducer.loadData(s);
-        })
-        .subscribe();
+    getStreamData()
+        .toStream()
+        .forEach(
+            s -> {
+              log.info("data from stream: {}", s);
+              kafkaProducer.loadData(s);
+            });
   }
 
   public Flux<String> getStreamData() {
-    return webClient.get().retrieve().bodyToFlux(String.class);
+    return webClient.get().retrieve().bodyToFlux(String.class)
+        .delayElements(Duration.ofMillis(100))
+        .doOnError(Throwable::printStackTrace)
+        .retry();
   }
 }
